d5e32624c968a00581e5ac2bfce2a9c8c073acce
mazax <mazaxguy@gmail.com>
2025-01-25 18:57:36 +0200

    Added speech bubbles

--- OccultFGJ/Assets/Scripts/DialogueManager.cs
+++ OccultFGJ/Assets/Scripts/DialogueManager.cs
@@ -8,10 +8,48 @@ using UnityEngine;
 
 public class DialogueManager : MonoBehaviour {
 
-	[SerializeField] private Text text;
-	private string path = "Assets/Text/dialogue.xml";
+	public struct Dialogue
+	{
+		public string speaker;
+		public string[] text;
+		public string[] choices;
+
+		public Dialogue(string speaker, string[] text, string[] choices) : this()
+		{
+			this.speaker = speaker;
+			this.text = text;
+			this.choices = choices;
+		}
+	}
+	[SerializeField] public Dialogue dialogue 
+		= new Dialogue("Leona", 
+						new string[] {"HISS"}, 
+						new string[] {"Choice1", "Choice2"});
+	[SerializeField] public GameObject speechBubble;
+	private Text textArea;
 	
-	private void Awake() {
+	public void StartDialogue(GameObject character)
+	{
+		speechBubble.SetActive(true);
+
+		textArea = speechBubble.GetComponentInChildren<Text>();
+		speechBubble.transform.position = Camera.main.WorldToScreenPoint(character.transform.position);
+		textArea.text = dialogue.text[0];
+	}
+	
+	public void ContinueDialogue()
+	{
+		// continue dialogue
+	}
+
+}
+
+
+
+/*
+void ReadXML() {
+
+		string path = "Assets/Text/dialogue.xml";
 		text = GetComponent<Text>();
 
         //Load
@@ -19,13 +57,21 @@ public class DialogueManager : MonoBehaviour {
 		{
 			while (reader.EndOfStream == false)
 			{
-				string line = reader.ReadToEnd();
-				text.text = line;
+				string lines = reader.ReadToEnd();
 
+				XmlDocument doc = new XmlDocument();
+				doc.LoadXml(lines);
 
+				XmlNode root = doc.FirstChild;
 
+				if (root.HasChildNodes)
+				{
+					for (int i = 0; i < root.ChildNodes.Count; i++)
+					{
+						Debug.Log(root.ChildNodes[i].InnerText);
+					}
+				}
 			}
 		}
     }
-
-}
+*/
