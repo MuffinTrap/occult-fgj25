cd603370eb35aa5f998bd665a38dc23c6df369da
mazax <mazaxguy@gmail.com>
2025-01-25 14:59:08 +0200

    Dialogue manager reading xml files

---
+++ OccultFGJ/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+	[SerializeField] private Text text;
+	private string path = "Assets/Text/dialogue.xml";
+	
+	private void Awake() {
+		text = GetComponent<Text>();
+
+        //Load
+		using (StreamReader reader = new StreamReader(path))
+		{
+			while (reader.EndOfStream == false)
+			{
+				string line = reader.ReadToEnd();
+				text.text = line;
+
+
+
+			}
+		}
+    }
+
+}
