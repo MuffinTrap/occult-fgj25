6db0cd04bdace99dd1150a9d5c85645c90a3d3fa
muffintrap ^ FCCCF <muffintrap@protonmail.com>
2025-01-25 15:17:07 +0200

    MusicPlayer can play 2 different tracks in cafe

--- OccultFGJ/Assets/Scripts/MusicPlayer.cs
+++ OccultFGJ/Assets/Scripts/MusicPlayer.cs
@@ -2,13 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicPlayer : MonoBehaviour {
-
+public class MusicPlayer : MonoBehaviour
+{
+	[SerializeField]
+	private AudioClip cafeMusic;
+	[SerializeField]
+	private AudioClip spookyMusic;
+	
+	private AudioSource musicSource;
 	
 	// Use this for initialization
 	void Start () {
-		AudioSource music = GetComponent<AudioSource>();
-		music.volume = PlayerPrefs.GetFloat("MusicVolume");
+		musicSource = GetComponent<AudioSource>();
+		musicSource.volume = PlayerPrefs.GetFloat("MusicVolume");
+		PlayCafeMusic();
+	}
+
+	public void PlayCafeMusic()
+	{
+		musicSource.clip = cafeMusic;
+		musicSource.Play();
+	}
+
+	public void PlaySpookyMusic()
+	{
+		musicSource.clip = spookyMusic;
+		musicSource.Play();
 	}
 	
 	// Update is called once per frame
