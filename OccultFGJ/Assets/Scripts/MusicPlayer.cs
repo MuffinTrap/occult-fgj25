c11ebf7e5a7da1b881f1c5125adad7dcee43eb27
muffintrap ^ FCCCF <muffintrap@protonmail.com>
2025-01-25 15:09:00 +0200

    Added cafe level and more music

---
+++ OccultFGJ/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	
+	// Use this for initialization
+	void Start () {
+		AudioSource music = GetComponent<AudioSource>();
+		music.volume = PlayerPrefs.GetFloat("MusicVolume");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
