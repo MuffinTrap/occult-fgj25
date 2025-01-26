37a8eecbe96a0a2131f3667c109cbda88b7fc64d
mazax <mazaxguy@gmail.com>
2025-01-26 12:32:07 +0200

    Multiple dialogues between characters

--- OccultFGJ/Assets/Scripts/Dialogue.cs
+++ OccultFGJ/Assets/Scripts/Dialogue.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +6,7 @@ public class Dialogue : MonoBehaviour {
 
 
 	public string speaker;
+	public GameObject speakerObject;
 	public string[] textLines;
 	public AudioClip[] audioLines;
 	public int lineIterator = 0;
---
+++ OccultFGJ/Assets/Scripts/DialogueTree.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTree : MonoBehaviour {
+
+	public Dialogue[] tree;
+	public int treeIterator = 0; 
+
+	public DialogueManager dialogueManager;
+
+	private void Start() {
+		dialogueManager = GameObject.Find("DialogueManager").GetComponent<DialogueManager>();
+	}
+
+	public Dialogue GetDialogue()
+	{
+		return tree[0];
+	}
+
+	public Dialogue NextLine()
+	{
+		Dialogue dialogue = tree[treeIterator];
+		dialogue.lineIterator++;
+		if (dialogue.lineIterator >= dialogue.textLines.Length)
+		{
+			dialogue = GetNextDialogue();
+		}
+		return dialogue;
+	}
+
+	public Dialogue GetNextDialogue()
+	{
+		treeIterator++;
+		if (treeIterator >= tree.Length)
+		{
+			treeIterator = 0;
+			dialogueManager.EndDialogue();
+			return null;
+		}
+		return tree[treeIterator];
+	}
+}
